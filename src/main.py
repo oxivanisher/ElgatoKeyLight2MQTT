a550a52d7f8bd7169d0939dd80c247eb10b9b02a
Marc Urben <aegnor@mittelerde.ch>
2024-10-04 17:54:08 +0200

    Update main.py
    hmm

--- src/main.py
+++ src/main.py
@@ -111,6 +111,7 @@ class KeyLight2MQTT:
         light_cleanup_timeout = 300  # Try to connect to lights and remove the ones not available
 
         if len(self.all_lights) and time.time() - self.last_light_cleanup > light_cleanup_timeout:
+            logging.info("Searching for disconnected lights")
             lights_to_remove = []
             for light in self.all_lights:
                 # if this light alredy is in the ingore list, skip it
